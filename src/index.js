6fc737e3ab1121f2a1586aa8416030f1c724e09a
kcliu <gjliou@cs.nctu.edu.tw>
2016-10-03 03:19:25 +0800

    fix iOS vh bug

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/layout/App/App.js';
-
+import { init } from 'viewport-units-buggyfill';
 import RoomA from './components/pages/roomA/index.js'
 //styles
 import './index.css';
 import 'animate.css/animate.min.css';
+
+init();
 
 ReactDOM.render(
   <App>
